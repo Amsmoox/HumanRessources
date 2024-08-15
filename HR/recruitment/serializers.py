03eac4cf5f8e7f2572ed7deb16e811a395d4cdad
amsmoox <amsmoox@gmail.com>
2024-08-15 22:17:37 +0100

    ApplicantSerializer

--- HR/recruitment/serializers.py
+++ HR/recruitment/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import JobPosting
+from .models import JobPosting, Applicant
 
 class JobPostingSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobPosting
+        fields = '__all__'
+
+class ApplicantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Applicant
         fields = '__all__'
