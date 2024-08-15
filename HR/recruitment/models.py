4d7f6f388a476adf5241f1d4068a7ff96341f35f
amsmoox <amsmoox@gmail.com>
2024-08-15 22:14:45 +0100

    JobPosting Model

--- HR/recruitment/models.py
+++ HR/recruitment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from employee_management.models import Employee
 
-# Create your models here.
+class JobPosting(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    posting_date = models.DateField(auto_now_add=True)
+    closing_date = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
