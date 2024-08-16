a70f5770d88070ab14978be4b345d4737d13aa3b
amsmoox <amsmoox@gmail.com>
2024-08-16 17:21:06 +0100

    ApplicantListCreateAPIView

--- HR/recruitment/api.py
+++ HR/recruitment/api.py
@@ -1,7 +1,12 @@
 from rest_framework import generics
-from .models import JobPosting
-from .serializers import JobPostingSerializer
+from .models import JobPosting, Applicant
+from .serializers import JobPostingSerializer, ApplicantSerializer
+
 
 class JobPostingListCreateAPIView(generics.ListCreateAPIView):
     queryset = JobPosting.objects.all()
     serializer_class = JobPostingSerializer
+
+class ApplicantListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Applicant.objects.all()
+    serializer_class = ApplicantSerializer
